b1fa8d4889e6f3969c44c77c1aeafd1ec3608828
LonelyWalker666 <1368539454@qq.com>
2019-05-22 21:45:07 +0800

    sadasd

--- core/src/com/bombcorps/game/model/Bomb.java
+++ core/src/com/bombcorps/game/model/Bomb.java
@@ -1,4 +1,8 @@
 package com.bombcorps.game.model;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Bomb {
+    private TextureRegion[] 尾翼;
+    private TextureRegion[] 爆炸
 }
--- core/src/com/bombcorps/game/model/World.java
+++ core/src/com/bombcorps/game/model/World.java
@@ -1,4 +1,6 @@
 package com.bombcorps.game.model;
 
 public class World {
+    砖块；
+
 }
