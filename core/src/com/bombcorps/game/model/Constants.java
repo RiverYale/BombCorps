5c4620432c8ed23d0afc89fe21200069200fbf5d
YaleRiver <1181065460@qq.com>
2019-05-22 22:48:40 +0800

    test

--- core/src/com/bombcorps/game/model/Constants.java
+++ core/src/com/bombcorps/game/model/Constants.java
@@ -1,6 +1,5 @@
 package com.bombcorps.game.model;
 
 public class Constants {
-    public static final float ACCELERATION = 5f;
-    public static final String HEROES_OBJECTS = "images/heroes.txt";
+    
 }
