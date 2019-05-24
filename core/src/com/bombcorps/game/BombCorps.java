95c48ba3ade86cab972b7fd413b628a38d7646dc
YaleRiver <1181065460@qq.com>
2019-05-24 23:54:06 +0800

    debug

--- core/src/com/bombcorps/game/BombCorps.java
+++ core/src/com/bombcorps/game/BombCorps.java
@@ -5,15 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.bombcorps.game.controller.InputController;
 
 public class BombCorps extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	public static int x = 0, y = 0, width = 300, height = 300;
+	public static float zoom = 1.0f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		Gdx.input.setInputProcessor(new GestureDetector(new InputController()));
 	}
 
 	@Override
@@ -21,7 +27,7 @@ public class BombCorps extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 1, 0.1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y, width, height);
 		batch.end();
 	}
 	
