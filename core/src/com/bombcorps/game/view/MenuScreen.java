b2270434788a004144858f7d71b9d03262e43e60
Gary <799494760@qq.com>
2019-05-25 12:26:12 +0800

    zwx

--- core/src/com/bombcorps/game/view/MenuScreen.java
+++ core/src/com/bombcorps/game/view/MenuScreen.java
@@ -3,6 +3,11 @@ package com.bombcorps.game.view;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
@@ -14,6 +19,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.bombcorps.game.model.Constants;
 
@@ -38,6 +44,7 @@ public class MenuScreen extends AbstractGameScreen {
 ////        }
         stage.act(deltaTime);
         stage.draw();
+        //Table.drawDebug(stage);
     }
     @Override public void resize(int width,int height){
         stage.getViewport().update(width,height);
@@ -59,11 +66,12 @@ public class MenuScreen extends AbstractGameScreen {
 
     // menu
     private Image imgBackground;
-    private Image imgLogo;
+    //private Image imgLogo;
     //private Image imgInfo;
 
-    private Button btnMenuPlay;
-    private Button btnMenuOptions;
+    private Image btnMenuPlay;
+    private Image btnMenuOptions;
+    private Image btnQuit;
 
     // options
     private Window winOptions;
@@ -87,7 +95,7 @@ public class MenuScreen extends AbstractGameScreen {
         // build all layers
         Table layerBackground = buildBackgroundLayer();
         //Table layerObjects = buildObjectsLayer();
-        Table layerLogos = buildLogosLayer();
+//      Table layerLogos = buildLogosLayer();
         Table layerControls = buildControlsLayer();
         Table layerOptionsWindow = buildOptionsWindowLayer();
         // assemble stage for menu screen
@@ -97,35 +105,71 @@ public class MenuScreen extends AbstractGameScreen {
         stack.setSize(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT);
         stack.add(layerBackground);
         //stack.add(layerObjects);
-        stack.add(layerLogos);
+        //stack.add(layerLogos);
         stack.add(layerControls);
         stage.addActor(layerOptionsWindow);
 
 
-        }
+    }
     private Table buildBackgroundLayer(){
         Table layer = new Table();
+        imgBackground = new Image(new TextureRegion(new Texture(Gdx.files.internal("background1.png")),0,0,1024,576));
+        layer.add(imgBackground);
         return layer;
     }
-//    private Table buildObjectsLayer(){
+    //    private Table buildObjectsLayer(){
 //        Table layer = new Table();
 //        return layer;
 //    }
-    private Table buildLogosLayer(){
-        Table layer = new Table();
-        return layer;
-    }
+//    private Table buildLogosLayer(){
+//        Table layer = new Table();
+//        return layer;
+//    }
     private Table buildControlsLayer(){
         Table layer = new Table();
+        layer.left().bottom();
+        //添加退出按钮
+        btnQuit = new Image(new Texture(Gdx.files.internal("button_quit.png")));
+        btnQuit.setSize(80,40);
+        layer.add(btnQuit);
+        btnQuit.addListener(new InputListener(){
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.exit();
+                return true;
+            }
+        });
+
+        layer.row().expandY();
+        //添加play按钮
+        btnMenuPlay = new Image(new Texture(Gdx.files.internal("button_start.png")));
+        btnMenuPlay.setSize(80,40);
+        layer.add(btnMenuPlay);
+        btnMenuPlay.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                onPlayClicked();
+            }
+        });
+        //添加设置按钮
+        btnMenuOptions = new Image(new Texture(Gdx.files.internal("button_setting.png")));
+        btnMenuOptions.setSize(80,40);
+        layer.add(btnMenuOptions);
         return layer;
     }
+
     private Table buildOptionsWindowLayer(){
         Table layer = new Table();
         return layer;
     }
 
+    private void onPlayClicked(){
+        //切换到LobbyScreen
+        //game.setScreen(new LobbyScreen(game));
+    }
 
+    private void onOptionsClicded(){
 
-
-
+    }
 }
