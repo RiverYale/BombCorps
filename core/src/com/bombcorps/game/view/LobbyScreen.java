799f857a954557c7acee5ce46a9d255baa6150b1
Gary <799494760@qq.com>
2019-05-26 22:35:12 +0800

    5.26

--- core/src/com/bombcorps/game/view/LobbyScreen.java
+++ core/src/com/bombcorps/game/view/LobbyScreen.java
@@ -3,13 +3,24 @@ package com.bombcorps.game.view;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
+import java.awt.Image;
+import java.awt.Label;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class LobbyScreen extends AbstractGameScreen{
     private static final String TAG = LobbyScreen.class.getName();
+
+    private Image lsBackground; //大厅背景
+    private Image perIfoBackground; //个人信息背景
+    private Image roomListBackground;
 
     private Stage perIfoStage;  //个人信息舞台
     private Stage roomListStage; //房间列表舞台
 
-
+    private Label labelShowName;    //昵称
+    private Label labelShowRate;    //胜率
+    private Label labelShowWinAmount;   //胜场
+    private Label labelShowProperty;    //金币数量
 
     public LobbyScreen(Game game) {
         super(game);
--- core/src/com/bombcorps/game/view/RoomSelect.java
+++ core/src/com/bombcorps/game/view/RoomSelect.java
@@ -1,4 +1,34 @@
 package com.bombcorps.game.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+
 public class RoomSelect {
+    private Image smallMap;
+    private Image background;
+    private String modern;
+    private String hosterName;
+    private String peopleNum;
+    private Label roomIfol;
+
+    RoomSelect(Image smallMap,String modern,String hosterName,String peopleNum){
+        this.smallMap = smallMap;
+        this.modern = modern;
+        this.hosterName = hosterName;
+        this.peopleNum = peopleNum;
+        background = new Image(new Texture(Gdx.files.internal("roombackground.png")));
+    }
+
+    public void setPosition(int x,int y){
+    }
+
+    public void addToStage(Stage stage){
+        stage.addActor(background);
+        stage.addActor(smallMap);
+        stage.addActor(roomIfol);
+    }
 }
