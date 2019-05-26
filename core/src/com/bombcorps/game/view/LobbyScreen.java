4ab615609b17c56f433c8911dda18341290ca2b6
Gary <799494760@qq.com>
2019-05-26 16:23:51 +0800

    5.26

--- core/src/com/bombcorps/game/view/LobbyScreen.java
+++ core/src/com/bombcorps/game/view/LobbyScreen.java
@@ -1,4 +1,42 @@
 package com.bombcorps.game.view;
 
-public class LobbyScreen {
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class LobbyScreen extends AbstractGameScreen{
+    private static final String TAG = LobbyScreen.class.getName();
+
+    private Stage perIfoStage;  //个人信息舞台
+    private Stage roomListStage; //房间列表舞台
+
+
+
+    public LobbyScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void render(float deltaTime) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
 }
