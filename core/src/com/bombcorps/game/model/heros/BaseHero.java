ba8c2de1bb70e11c0d8ff5f5f533f0d4ce5299b3
LonelyWalker666 <1368539454@qq.com>
2019-06-07 15:57:35 +0800

    6/7

--- core/src/com/bombcorps/game/model/heros/BaseHero.java
+++ core/src/com/bombcorps/game/model/heros/BaseHero.java
@@ -177,7 +177,7 @@ public class BaseHero{
         dimension = Constants.HERO_DIMENSION;
         origin = new Vector2(dimension.x / 2, dimension.y / 2);
         scale = new Vector2(1,1);
-        velocity = new Vector2();
+        velocity = new Vector2(Constants.VELOCITY_X, 0);
         acceleration = new Vector2(0,Constants.ACCELERATION);
         rec = new Rectangle(0,0,dimension.x,dimension.y);
 
