b095b8c25925972b23d906e4c0d1b04ac988d2db
imcerde <32219000+imcerde@users.noreply.github.com>
2019-06-09 15:13:57 +0800

    Q

--- core/src/com/bombcorps/game/view/GameScreen.java
+++ core/src/com/bombcorps/game/view/GameScreen.java
@@ -330,17 +330,29 @@ public class GameScreen extends AbstractGameScreen{
             bar.setY(i*15+1);
             if(i == 2){
                 bar.setSize(width/15*1.5f*myPlayer.getMyHero().getHealth()/myPlayer.getMyHero().getMaxHealth(), bar.getHeight());
+                bar.draw(batch);
+                font.getData().setScale(0.4f);
+                font.draw(batch,(int)(myPlayer.getMyHero().getHealth())+"/"+(int)(myPlayer.getMyHero().getMaxHealth()),imgMyHeroHead.getX()+43*scale,bar.getY()+8);
             }else if(i == 1){
                 bar.setSize(width/15*1.5f*myPlayer.getMyHero().getEndurance()/Constants.MAX_ENDURENCE,bar.getHeight());
+                bar.draw(batch);
+                font.getData().setScale(0.4f);
+                font.draw(batch,(int)(myPlayer.getMyHero().getEndurance())+"/"+(int)(Constants.MAX_ENDURENCE),imgMyHeroHead.getX()+43*scale,bar.getY()+8);
             }else if(i==0){
                 bar.setSize(width/15*1.5f*myPlayer.getMyHero().getRagePower()/Constants.MAX_RAGEPOWER,bar.getHeight());
+                bar.draw(batch);
+                font.getData().setScale(0.4f);
+                font.draw(batch,(int)(myPlayer.getMyHero().getRagePower())+"/"+(int)(Constants.MAX_RAGEPOWER),imgMyHeroHead.getX()+43*scale,bar.getY()+8);
+
             }
-            bar.draw(batch);
+
         }
     }
 
     public void batchOtherHeroInfo(SpriteBatch batch){
         imgOtherHeroHead.draw(batch);
+        font.getData().setScale(0.6f);
+        font.draw(batch,otherPlayer.getID(),imgOtherHeroHead.getX(),60);
         bar.setX(imgOtherHeroHead.getX()+43*scale);
         for(int i=2;i>=0;i--) {
             bar.setColor(Color.GRAY);
@@ -352,13 +364,21 @@ public class GameScreen extends AbstractGameScreen{
             bar.setY(i*15+1);
             if(i == 2){
                 bar.setSize(width/15*1.5f*otherPlayer.getMyHero().getHealth()/otherPlayer.getMyHero().getMaxHealth(), bar.getHeight());
+                bar.draw(batch);
+                font.getData().setScale(0.4f);
+                font.draw(batch,(int)(otherPlayer.getMyHero().getHealth())+"/"+(int)(otherPlayer.getMyHero().getMaxHealth()),imgOtherHeroHead.getX()+43*scale,bar.getY()+8);
             }else if(i == 1){
                 bar.setSize(width/15*1.5f*otherPlayer.getMyHero().getEndurance()/Constants.MAX_ENDURENCE,bar.getHeight());
-
+                bar.draw(batch);
+                font.getData().setScale(0.4f);
+                font.draw(batch,(int)(otherPlayer.getMyHero().getEndurance())+"/"+(int)(Constants.MAX_ENDURENCE),imgOtherHeroHead.getX()+43*scale,bar.getY()+8);
             }else {
                 bar.setSize(width/15*1.5f*otherPlayer.getMyHero().getRagePower()/Constants.MAX_RAGEPOWER,bar.getHeight());
+                bar.draw(batch);
+                font.getData().setScale(0.4f);
+                font.draw(batch,(int)(otherPlayer.getMyHero().getRagePower())+"/"+(int)(Constants.MAX_RAGEPOWER),imgOtherHeroHead.getX()+43*scale,bar.getY()+8);
             }
-            bar.draw(batch);
+
         }
     }
 
